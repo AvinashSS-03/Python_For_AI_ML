36816169e3d9fff38819a648fbbfbac048660ec6
Avinash <avinashssathyakumar@gmail.com>
2026-06-10 15:22:10 +0530

    Basic problems

---
+++ basic_math/odd_even.py
@@ -0,0 +1,14 @@
+def Wloop():
+    a=25
+    while(a<=50):
+        if a%2!=0:
+            print("The number is odd",a)
+        a+=1 #26
+Wloop()
+def floop():
+    for i in range(25,50+1):
+        if i%2!=0:
+            print("The number is even",i,end=" ")
+
+floop()
+
