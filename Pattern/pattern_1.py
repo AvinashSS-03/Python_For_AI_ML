59d81243906949518df2d5228cd943f7e9f81330
Avinash <avinashssathyakumar@gmail.com>
2026-06-12 10:01:54 +0530

    Pattern problems

--- Pattern/pattern_1.py
+++ Pattern/pattern_1.py
@@ -3,3 +3,17 @@ for i in range(1,a+1):
     for j in range(1,a+1):
         print(i,end="")
     print()
+
+print("while loop")
+
+def wloop():
+    a=1
+    while(a<=4):
+        b=1
+        while(b<=4):
+            print(a,end=" ")
+            b+=1
+        a+=1
+        print()
+
+wloop()
