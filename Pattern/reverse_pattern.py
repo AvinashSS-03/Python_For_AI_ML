01e929d33bd4ef32f814fb92e7430ed222edf07e
Avinash <avinashssathyakumar@gmail.com>
2026-06-12 10:02:19 +0530

    Pattern problems

--- Pattern/reverse_pattern.py
+++ Pattern/reverse_pattern.py
@@ -3,3 +3,17 @@ for i in range(a,0,-1):
     for j in range(1,i+1):
         print("*",end=" ")
     print()
+
+print("while loop")
+
+
+def wloop():
+    a=4
+    while(a>0):
+        b=1
+        while(b<=a):
+            print("*",end=" ")
+            b+=1
+        print()
+        a-=1
+wloop()
