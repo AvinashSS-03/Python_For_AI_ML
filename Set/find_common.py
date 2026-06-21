77423068ff07bbce5d1431135f9df88c728a52de
Avinash <avinashssathyakumar@gmail.com>
2026-06-21 15:02:05 +0530

    Set problems

---
+++ Set/find_common.py
@@ -0,0 +1,7 @@
+#This program is used to find the common elements
+
+class_a = {"avinash", "ram", "kumar"}
+class_b = {"ram", "surya", "kumar"}
+
+common=class_a.intersection(class_b)
+print(common)
