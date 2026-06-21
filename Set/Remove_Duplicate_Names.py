95ac32731acdaf20804e63caacb98a3b8c78e99e
Avinash <avinashssathyakumar@gmail.com>
2026-06-21 15:02:15 +0530

    Set problems

---
+++ Set/Remove_Duplicate_Names.py
@@ -0,0 +1,9 @@
+n=("Apple","orange","Apple","Grapes","lemon")
+print(set(n))
+#This problem is used to remove duplicates in the list using set
+
+#user input store in list
+
+nums=input("enter names: ").split()
+
+print(nums)
