64953ea06cb82d31831998808cac0da757dc8ffa
Avinash <avinashssathyakumar@gmail.com>
2026-06-23 15:17:53 +0530

    Oops problem

---
+++ oops/constructor.py
@@ -0,0 +1,16 @@
+#using constructor a small student apllication
+
+
+class student:
+    def __init__(self,name,id):
+        self.student_name=name
+        self.student_id=id
+    def display(self):
+        print("Student name: ",self.student_name)
+        print("Student id: ",self.student_id)
+
+name=input("Enter student name: ")
+id=int(input("Enter student id: "))
+
+obj=student(name,id)
+obj.display()
