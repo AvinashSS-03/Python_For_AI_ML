93666fea586abcaaaeb51611ec2268a58e5c007d
Avinash <avinashssathyakumar@gmail.com>
2026-06-26 23:02:05 +0530

    Exception Handling

--- Exceptionhandling/ExceptionHandling.py
+++ Exceptionhandling/ExceptionHandling.py
@@ -1,8 +1,8 @@
 # a="number" #-->This raises a TypeError
   #instead of string if you give anything then that raises value error
 try:
-    a = int(input("Enter  a numer"))
-    a/5
+    num = int(input("Enter  a numer"))
+    num/5
     #a/0 -->this return It is not divisble
 except ValueError:
     print("enter a integer")
