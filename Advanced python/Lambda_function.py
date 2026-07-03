c7ed0c59ce980a44feb13dc13b750a4670815c01
Avinash <avinashssathyakumar@gmail.com>
2026-07-03 20:48:17 +0530

    Advanced Python

---
+++ Advanced python/Lambda_function.py
@@ -0,0 +1,13 @@
+#lambda is a small function that can have any number of arguments but ony one expresiion
+#syntax:
+#lambda arguments :expresiion
+
+#simple square function
+#normal function
+def square(x):
+    return x * x
+print(square(4))
+#lambda function
+square=lambda x:x*x
+print(square(5))
+
