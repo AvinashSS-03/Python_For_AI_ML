f1a99c7c4b986abd2e3f2c32bd90d3f8ef94319a
Avinash <avinashssathyakumar@gmail.com>
2026-07-05 23:13:17 +0530

    GUI

---
+++ GUI/__init__.py
@@ -0,0 +1,7 @@
+#Tkinter is Python's built-in library for creating GUI (Graphical User Interface) applications.
+# Instead of using only the terminal, you can create windows with buttons, text boxes, labels, menus, and more.
+#A GUI (Graphical User Interface) lets users interact with a program using graphical elements like:
+from tkinter import *
+s=Tk() # -->class for window page
+s.mainloop() # --> to view
+#labels,buttom,radiobutton,Checkbutton
