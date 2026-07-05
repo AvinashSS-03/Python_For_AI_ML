8ed158431aaa6884a4b8f2dfec9f54c1a1dd7b19
Avinash <avinashssathyakumar@gmail.com>
2026-07-05 23:19:56 +0530

    GUI

---
+++ GUI/Button.py
@@ -0,0 +1,11 @@
+from tkinter import *
+#buuton is used to create a button so whenever we click some function is to be done
+#coomand -->call functions
+#quit -> exit the code
+root=Tk()
+def greet():
+    print("Welcome to python")
+Button(text="Click",command=greet).place(x=65,y=63)
+Button(text="Exit",command=quit).place(x=25,y=93)
+
+root.mainloop()
