cc038bf7caa5759c9cacc4e8bfadd3826c0f7d1f
Avinash <avinashssathyakumar@gmail.com>
2026-07-09 22:32:11 +0530

    Booked details and Payment summary

--- Mini_project_2/Payment.py
+++ Mini_project_2/Payment.py
@@ -1,7 +1,6 @@
 from tkinter import *
 import sqlite3
-com=sqlite3.connect("DataBase")
-cur=com.cursor()
+import  Movie_Ticket_booking_system
 import Data
 
 def payment_now():
@@ -9,7 +8,7 @@ def payment_now():
     Label(text="Billing system", fg="black", bg="yellow",
           font=("Times New Roman", 30, "bold")).place(x=500, y=100)
 
-    cur.execute("""
+    Movie_Ticket_booking_system.cur.execute("""
     INSERT INTO Database
     (user_id,selected_movie,
      selected_theater, selected_timing,
@@ -23,11 +22,37 @@ def payment_now():
         ",".join(Data.selected_seat),
         Data.Amount
     ))
-    com.commit()
+    Movie_Ticket_booking_system.com.commit()
 
+    bill = f"""
+     ========================================================
+                     MOVIE TICKET BOOKING                    
+     ========================================================
+      Customer Name : {Data.username:<35}
+     --------------------------------------------------------
+      Movie         : {Data.movie:<35}
+      Theater       : {Data.theater:<35}
+      Show Time     : {Data.timing:<35}
+      Seats         : {", ".join(Data.selected_seat):<35}
+     --------------------------------------------------------
+      Ticket Price  : ₹100                                   
+      No. of Seats  : {len(Data.selected_seat):<35}
+    --------------------------------------------------------
+     Total Amount  : ₹{Data.Amount:<34}
+     ========================================================
+                   THANK YOU! VISIT AGAIN                    
+     ========================================================
+    """
+    Label(
+    root,
+    text=bill,
+    font=("Times New Roman", 12, "bold"),
+    bg="white",
+    fg="black"
+    ).place(x=500, y=200)
 
+    Movie_Ticket_booking_system.prints()
 
 
     root.geometry("1920x1080")
     root.mainloop()
-#payment_now()
